42b4e8f9bd3a825df32ba9a3044d17ca18b73c9e
Douglas Molina <douglas.molina@va.gov>
2020-06-21 23:20:02 -0500

    Update PySilo3Rev-2.py

--- PySilo3Rev-2.py
+++ PySilo3Rev-2.py
@@ -160,7 +160,7 @@ def misp_check_for_previous_event(misp_instance, isight_alert):
         result = misp_instance.search(value=isight_alert.Id, type_attribute='text', category='External analysis')
         # If something was found in the MISP instance, then retrieve the event
        
-    if result:
+        if result:
             event = check_misp_all_results(result)
 
     # If no event found, search based on report URL.
