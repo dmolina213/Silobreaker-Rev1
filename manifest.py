48f34a4915898bc782b0fc0452e290575b24d2f1
Douglas Molina <douglas.molina@va.gov>
2020-06-24 15:29:28 -0500

    Update manifest.py

--- manifest.py
+++ manifest.py
@@ -1,3 +1,146 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+#PySilo3beta-2.py
+#dmolina213
+#1*****
+import datetime
+
+import email.utils
+
+import hashlib
+
+import hmac
+
+import json
+
+import os
+
+from pymisp import ExpandedPyMISP, MISPEvent, MISPObject,PyMISP
+
+#from pymisp import PyMISP, MISPEvent, MISPObject
+
+import requests
+
+import sys
+
+import threading
+
+import time
+
+import urllib.parse
+
+import urllib3
+
+
+
+# Read the config file.
+
+import PySilo_settings
+
+
+
+# Import our own iSight report model.
+
+from model.PySiloReport import PySiloReport
+
+#from .models import PySiloReport
+
+
+
+# Suppress insecure HTTPS request warnings.
+
+urllib3.disable_warnings()
+
+
+
+
+
+# Error handling function.
+
+def error_handling(e, a_string):
+
+    """
+    :param e:
+    :type e:
+    :param a_string:
+    :type a_string:
+    :return:
+    :rtype:
+    """
+    if hasattr(e, 'Items'):
+        PySilo_settings.logger.debug('%s %s', a_string, e.Items)
+    import traceback
+    PySilo_settings.logger.debug('1 %s', e.__doc__)
+    PySilo_settings.logger.debug('2 %s', sys.exc_info())
+    PySilo_settings.logger.debug('3 %s', sys.exc_info()[0])
+    PySilo_settings.logger.debug('4 %s', sys.exc_info()[1])
+    #PySilo_settings.logger.debug('5 %s', sys.exc_info()[2], 'Sorry I mean line...',
+    #                              traceback.tb_lineno(sys.exc_info()[2]))
+    ex_type, ex, tb = sys.exc_info()
+    PySilo_settings.logger.debug('6 %s', traceback.print_tb(tb))
+    return sys, traceback
+
+
+
+
+
+
+import base64
+
+import hashlib
+
+import hmac
+
+import html
+
+import json
+
+import urllib.request
+
+import argparse
+
+from urllib import parse
+
+
+
+# Process all FireEye iSight reports and convert them to MISP events.
+
+
+def misp_process_isight_indicators(a_result):
+    
+    """
+    :param a_result:
+    :type a_result:
+    """
+    PySilo_settings.logger.debug('misp_process_isight_indicators')
+
+    # Process each indicator in the JSON message
+
+    for indicator in a_result['Items']:
+        Print("#####indicator#####",indicator)
+        PySilo_settings.logger.debug('Processing report %s', indicator['Id'])
+
+        if PySilo_settings.use_threading:
+            # Use threads to process the indicators
+            print('***threading****')
+            # First, set the maximum number of threads
+            thread_limiter = threading.BoundedSemaphore(value=PySilo_settings.number_threads)
+            # Define a thread
+            t = threading.Thread(target=process_isight_indicator, args=(indicator,))
+            # Start the thread
+            t.start()
+        else:
+            # No threading
+            print('***no threading***')
+            process_isight_indicator(indicator)
+
+
+
+
+
+
 ################################start################################
 # Command line arguments 
 
