05c18e32a8880366727fde9814eec6f16fc7fb9e
Douglas Molina <douglas.molina@va.gov>
2020-06-25 13:40:50 -0500

    Update PySilobeta-3.py

--- PySilobeta-3.py
+++ PySilobeta-3.py
@@ -275,14 +275,14 @@ def update_misp_event(misp_instance, event, isight_alert):
         if isight_alert.Id:
             email_object.add_attribute('subject', value=isight_alert.Id, to_ids=False)
         if isight_alert.recipient:
-            #email_object.add_attribute('to', value=isight_alert.recipient, to_ids=False)
-        if isight_alert.senderDomain:
-            domain_attribute = event.add_attribute(category='Network activity', type='domain',value=isight_alert.senderDomain, to_ids=False)
-            email_object.add_reference(domain_attribute.uuid, 'derived-from', comment='Email source domain')
+            email_object.add_attribute('to', value=isight_alert.recipient, to_ids=False)
+        #if isight_alert.senderDomain:
+           # domain_attribute = event.add_attribute(category='Network activity', type='domain',value=isight_alert.senderDomain, to_ids=False)
+           # email_object.add_reference(domain_attribute.uuid, 'derived-from', comment='Email source domain')
         # Lastly, add the object to the event.
-        PySilo_settings.logger.debug('Adding object to event %s',email_object)
-        print('Adding object to event ',email_object)
-        event.add_object(email_object)
+   PySilo_settings.logger.debug('Adding object to event %s',email_object)
+   print('Adding object to event ',email_object)
+   event.add_object(email_object)
 
     # If the report contains an MD5 hash, create a file object.
    if isight_alert.md5:
