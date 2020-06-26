696c1f9571b99719479f48203f6669a3a2634722
Douglas Molina <douglas.molina@va.gov>
2020-06-26 14:39:32 -0500

    Update PySilobeta-4.py

--- PySilobeta-4.py
+++ PySilobeta-4.py
@@ -155,8 +155,9 @@ def misp_check_for_previous_event(misp_instance, isight_alert):
         return False
 
     #Search based on report ID.
-    if isight_alert.Id:
-        result = misp_instance.search(value=isight_alert.Id, type_attribute='text', category='External analysis')
+    if isight_alert.Description:
+        result = misp_instance.search(value=isight_alert.Description, type_attribute='text', category='Payload delivery')
+        PySilo_settings.logger.debug('160 event found %s',result)
         # If something was found in the MISP instance, then retrieve the event
        
         if result:
